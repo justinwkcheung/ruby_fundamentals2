5fb7ffb043e59c6ff44f007c7fd0b057979ed141
Justin Cheung <justinwkcheung@github.com>
2017-01-17 14:25:23 -0500

    Complete questions 3-6

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,20 @@
 grocery_list = ["* carrots", "* toilet paper", "* apples", "* salmon"]
 
-
 grocery_list << "* rice"
 
 def show_list(a)
   puts a
 end
 
+show_list(grocery_list)
+
+show_list(grocery_list.count)
+
+show_list(grocery_list.include?("bananas"))
+
+show_list("Second grocery item in list is #{grocery_list[1]}")
+
+show_list(grocery_list.sort)
+
+grocery_list.delete("* salmon")
 show_list(grocery_list)
