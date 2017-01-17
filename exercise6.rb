c1ff54a653780d3d1b229d39011798347cf49d04
Justin Cheung <justinwkcheung@github.com>
2017-01-17 14:08:59 -0500

    Create method to print out grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,10 @@
 grocery_list = ["* carrots", "* toilet paper", "* apples", "* salmon"]
-puts grocery_list
+
+
+grocery_list << "* rice"
+
+def show_list(a)
+  puts a
+end
+
+show_list(grocery_list)
