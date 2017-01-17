6b6bf841557076bb32d7b570739f9bd7b526dde9
Justin Cheung <justinwkcheung@github.com>
2017-01-17 15:16:02 -0500

    Complete questions 3-6

--- exercise7.rb
+++ exercise7.rb
@@ -6,8 +6,25 @@ students = {
 
 def cohort_name_number(cohorts)
   cohorts.each do |x, y|
-    puts "#{x}: #{y}"
+    puts "#{x}: #{y} students"
   end
 end
+
+cohort_name_number(students)
+
+students[:cohort4] = 43
+cohort_name_number(students)
+
+puts students.keys
+
+def increased_size(cohorts)
+  cohorts.each do |x, y|
+    puts "#{x}: #{y * 1.05} students"
+  end
+end
+
+increased_size(students)
+
+students.delete(:cohort2)
 
 cohort_name_number(students)
