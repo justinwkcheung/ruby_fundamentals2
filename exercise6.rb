d24595de9f1f8ef05879279c58bf53db274a7659
Justin Cheung <justinwkcheung@github.com>
2017-01-17 14:44:56 -0500

    Recomplete exercise 6 as first completion is incorrect

--- exercise6.rb
+++ exercise6.rb
@@ -1,20 +1,45 @@
-grocery_list = ["* carrots", "* toilet paper", "* apples", "* salmon"]
-
-grocery_list << "* rice"
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 def show_list(a)
-  puts a
+  a.each do |item|
+    puts "* #{item}"
+  end
 end
 
 show_list(grocery_list)
 
-show_list(grocery_list.count)
+grocery_list << "rice"
 
-show_list(grocery_list.include?("bananas"))
+show_list(grocery_list)
 
-show_list("Second grocery item in list is #{grocery_list[1]}")
+puts grocery_list.count
+
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas"
+end
+
+puts grocery_list[1]
 
 show_list(grocery_list.sort)
 
-grocery_list.delete("* salmon")
+grocery_list.delete("salmon")
 show_list(grocery_list)
+
+# def show_list(a)
+#   puts a
+# end
+#
+# show_list(grocery_list)
+#
+# show_list(grocery_list.count)
+#
+#
+# show_list("Second grocery item in list is #{grocery_list[1]}")
+#
+# show_list(grocery_list.sort)
+#
+# grocery_list.delete("* salmon")
+# show_list(grocery_list)
